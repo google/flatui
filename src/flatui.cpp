4872f044924ecc9329a1f8dce4f73f7729d0fd7d
Jason Sanmiya <jsanmiya@google.com>
2015-10-28 19:27:15 -0700

    Add #include that was missing when SIMD disabled on mathfu.
    When SIMD is enabled, extra headers are included from vectorial.
    We were missing one of these when SIMD was not enabled.
    
    Tested: Builds on Linux without SIMD support now.
    
    Change-Id: Ib9ae6daf917ef451b40e9aa065f73dc94abfc88e

--- src/flatui.cpp
+++ src/flatui.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstring>
 #include "fplbase/utilities.h"
 #include "flatui/flatui.h"
 #include "flatui/internal/flatui_util.h"
