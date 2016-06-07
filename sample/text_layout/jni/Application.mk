14eee952358dec482859d64c71cf10c4903d2230
Stewart Miles <smiles@google.com>
2016-06-07 13:18:29 -0700

    Switch from the deprecated gnustl runtime to libc++.
    Also, patched the Ant build to ignore spurious errors from ndk-build
    about LOCAL_LDLIBS being set when using libc++.
    
    NOTE: This results in spurious warnings of the form
    "LOCAL_LDLIBS is always ignored for static libraries" due to the
    use of libc++.
    
    Change-Id: Iebb92e6e1a4b4905b1274d22aeda044a2dcff0a4

--- sample/text_layout/jni/Application.mk
+++ sample/text_layout/jni/Application.mk
@@ -14,7 +14,7 @@
 
 APP_PLATFORM := android-15
 APP_ABI:=armeabi armeabi-v7a mips x86 x86_64
-APP_STL := gnustl_static
+APP_STL:=c++_static
 APP_MODULES := main
 
 APP_CPPFLAGS += -std=c++11 -Wno-literal-suffix
