4598e7d9c9ad30559ac770a1cb90197ffd9ec4f0
laijess <laijess@google.com>
2017-01-17 17:48:01 +0000

    Add use of spring animation to CorgUI sample.
    Add use of spring animation to give the sample's heart sprites more exciting
    animation patterns.
    PiperOrigin-RevId: 144720426
    Change-Id: I4a2989170229cfd42262c34c25493825239afe07

--- sample/corgui/corgui.cpp
+++ sample/corgui/corgui.cpp
@@ -24,6 +24,7 @@
 
 using flatui::AnimCurveDescription;
 using flatui::kAnimEaseInEaseOut;
+using flatui::kAnimSpring;
 using flatui::EndGroup;
 using flatui::HashedId;
 using flatui::Margin;
@@ -66,15 +67,17 @@ static const AnimCurveDescription kScoreSizeCurveDescription(kAnimEaseInEaseOut,
 static const AnimCurveDescription kPointSizeCurveDescription(kAnimEaseInEaseOut,
                                                              10.0f, 2000.0f,
                                                              0.5f);
+static const AnimCurveDescription kSpringCurveDescription(kAnimSpring, 20.0f,
+                                                          2000.0f, 0.5f);
 
 // Constant variables to be used.
 static const vec2i kWindowSize = vec2i(1080, 1920);
 static const vec4 kWhiteColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 static const vec4 kGreyColor = vec4(0.5f, 0.5f, 0.5f, 0.5f);
 static const vec4 kTransparent = vec4(0.0f, 0.0f, 0.0f, 0.0f);
-static const vec2 kPointsPositionTarget = vec2(100.0f, -890.0f);
-static const vec2 kPointsPositionStart = vec2(0.0f, -60.0f);
 static const vec2 kScoreLabelOffset = vec2(10.0f);
+static const float kSpriteYTargetPosition = -890.0f;
+static const float kSpriteYStartPosition = -60.0f;
 static const float kYVirtualResolution = 1080.0f;
 static const float kCorgiBodySize = 600.0f;
 static const float kCorgiHornSize = 400.0f;
@@ -90,7 +93,7 @@ static const float kYPointsSizeStart = 20.0f;
 static const int kEvolutionPoint = 100;
 static const int kClickScore = 10;
 static const char kCorguiTitleBanner[] = "CorgUI";
-static const char kPointsId[] = "points_ascending";
+static const char kSpriteYPosId[] = "sprites_ascending";
 static const char kFlashId[] = "text_flashing";
 static const char kSizeId[] = "label_size_changing";
 static const char kCurveId[] = "text_curving";
@@ -98,6 +101,7 @@ static const char kTextColorId[] = "text_color_changing";
 static const char kScoreColorId[] = "score_color_changing";
 static const char kScoreId[] = "score_size_changing";
 static const char kLoadingString[] = "Loading CorgUI";
+static const char kSpringId[] = "spring_x";
 static const vec4 kTextColors[] = {
     vec4(1.0f, 1.0f, 1.0f, 1.0f), vec4(0.87f, 0.52f, 0.54f, 1.0f),
     vec4(0.0f, 1.0f, 0.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f),
@@ -112,6 +116,9 @@ static const char *kCorgiTextureNames[] = {
     "textures/corgi_wings.webp",         // kCorgiTextureWingedBody
     "textures/heart_icon.webp",          // kCorgiTextureHeartIcon
 };
+static const float kSpriteXStartPositions[] = {
+    30.0f, 140.0f, 60.0f, 100.0f, 80.0f,
+};
 static const char kFontPath[] = "fonts/LuckiestGuy.ttf";
 static_assert(FPL_ARRAYSIZE(kCorgiTextureNames) == kCorgiTextureCount,
               "kCorgiTextureNames not consistent with CorgiTexture enum");
@@ -119,16 +126,20 @@ static_assert(FPL_ARRAYSIZE(kCorgiTextureNames) == kCorgiTextureCount,
 // Struct to hold the state of the game.
 struct CorgiGameState {
   CorgiGameState()
-      : score(0),
+      : score_text_color_target(0),
+        sprite_start(0),
+        score(0),
         score_label_size(kDefaultLabelSize),
         score_label_target(kDefaultLabelSize),
-        score_text_color(kWhiteColor),
-        score_text_color_target(0) {}
+        movement_sign(-1.0f),
+        score_text_color(kWhiteColor) {}
+  int score_text_color_target;
+  int sprite_start;
   int score;
   float score_label_size;
   float score_label_target;
+  float movement_sign;
   vec4 score_text_color;
-  int score_text_color_target;
 };
 
 // Helper functions to choose what textures and sizes to use.
@@ -298,6 +309,10 @@ extern "C" int FPL_main(int /*argc*/, char **argv) {
         // Increment the score.
         curr_game.score += kClickScore;
         // Flip the movement sign.
+        curr_game.movement_sign *= -1.0f;
+        // Update heart start position.
+        curr_game.sprite_start = (curr_game.sprite_start + 1) %
+                                 FPL_ARRAYSIZE(kSpriteXStartPositions);
 
         // Animate the score label size changing.
         curr_game.score_label_target =
@@ -312,14 +327,13 @@ extern "C" int FPL_main(int /*argc*/, char **argv) {
         // Add points and heart sprites. Both will grow and float upwards
         // until offscreen. The points will fade in color until transparent.
         SequenceId seq = flatui::AddSprite(
-            kPointsId, [&corgi_textures, &curr_game](SequenceId seq) -> bool {
-              // Create a hash for this specific sprite.
-              const HashedId curr_sprite_hash =
-                  flatui::HashedSequenceId(kPointsId, seq);
-
-              // Set up the animatable.
-              const vec2 sprite_position = flatui::Animatable<vec2>(
-                  curr_sprite_hash, kPointsPositionStart);
+            kSpriteYPosId,
+            [&corgi_textures, &curr_game](SequenceId seq) -> bool {
+              // Set up the animatable for the sprites' y positions.
+              const HashedId y_position_sprite_hash =
+                  flatui::HashedSequenceId(kSpriteYPosId, seq);
+              const float sprite_position = flatui::Animatable<float>(
+                  y_position_sprite_hash, kSpriteYStartPosition);
 
               // Change the text size.
               const HashedId label_size_sprite_hash =
@@ -333,13 +347,19 @@ extern "C" int FPL_main(int /*argc*/, char **argv) {
               const vec4 text_color =
                   flatui::Animatable<vec4>(text_color_sprite_hash, kWhiteColor);
 
-              // Start the group.
+              // Start the sprite's group.
               StartGroup(flatui::kLayoutVerticalCenter, 0,
                          CalculateStringFromInt(seq).c_str());
               // Position the group to where the animation currently has
-              // sprite_position.
+              // the spring animation and sprite_position.
+              const HashedId spring_sprite_hash =
+                  flatui::HashedSequenceId(kSpringId, seq);
+              const float curr_sprite_x = flatui::Animatable(
+                  spring_sprite_hash,
+                  -curr_game.movement_sign *
+                      kSpriteXStartPositions[curr_game.sprite_start]);
               PositionGroup(flatui::kAlignCenter, flatui::kAlignCenter,
-                            sprite_position);
+                            vec2(curr_sprite_x, sprite_position));
               flatui::SetTextColor(text_color);
               flatui::Label(CalculateStringFromInt(kClickScore).c_str(),
                             label_size);
@@ -348,16 +368,25 @@ extern "C" int FPL_main(int /*argc*/, char **argv) {
               EndGroup();
 
               const bool no_time_remaining =
-                  flatui::AnimationTimeRemaining(curr_sprite_hash) <= 0 &&
+                  flatui::AnimationTimeRemaining(y_position_sprite_hash) <= 0 &&
                   flatui::AnimationTimeRemaining(label_size_sprite_hash) <= 0 &&
-                  flatui::AnimationTimeRemaining(text_color_sprite_hash) <= 0;
+                  flatui::AnimationTimeRemaining(text_color_sprite_hash) <= 0 &&
+                  flatui::AnimationTimeRemaining(spring_sprite_hash) <= 0;
               return no_time_remaining;
             });
-        // Find and start points sprite animation.
-        const HashedId points_sprite_hash =
-            flatui::HashedSequenceId(kPointsId, seq);
-        flatui::StartAnimation<vec2>(points_sprite_hash, kPointsPositionTarget,
-                                     mathfu::kZeros2f, kSpriteCurveDescription);
+        // Find and start position sprite animation.
+        const HashedId y_position_sprite_hash =
+            flatui::HashedSequenceId(kSpriteYPosId, seq);
+        const HashedId spring_sprite_hash =
+            flatui::HashedSequenceId(kSpringId, seq);
+        flatui::StartAnimation<float>(y_position_sprite_hash,
+                                      kSpriteYTargetPosition, 0.0f,
+                                      kSpriteCurveDescription);
+        flatui::StartAnimation<float>(
+            spring_sprite_hash,
+            curr_game.movement_sign *
+                kSpriteXStartPositions[curr_game.sprite_start],
+            0.0f, kSpringCurveDescription);
 
         // Find and start the size sprite animations.
         const HashedId size_sprite_hash =
@@ -371,7 +400,7 @@ extern "C" int FPL_main(int /*argc*/, char **argv) {
         flatui::StartAnimation<vec4>(text_color_sprite_hash, kTransparent,
                                      mathfu::kZeros4f, kColorCurveDescription);
 
-        // Find and start the flash sprite animations, if we've hit a thousand
+        // Find and start the flash sprite animations, if we've hit an evolution
         // point mark.
         if (curr_game.score % kEvolutionPoint == 0) {
           curr_game.score_text_color_target =
@@ -384,7 +413,7 @@ extern "C" int FPL_main(int /*argc*/, char **argv) {
         }
       }
       // Draw any sprites that may have been added.
-      flatui::DrawSprites(kPointsId);
+      flatui::DrawSprites(kSpriteYPosId);
       flatui::DrawSprites(kFlashId);
       flatui::DrawSprites(kSizeId);
       flatui::DrawSprites(kCurveId);
